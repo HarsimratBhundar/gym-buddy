2985299bfb5b73714fdf341aab064fa7d39b84de
J <J@Js-MacBook-Pro.local>
2020-03-11 22:49:15 -0400

    created login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes } from "@angular/router";
 
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
+import { LoginComponent } from "~/app/login/login.component";
 
 const routes: Routes = [
     { path: "", redirectTo: "/items", pathMatch: "full" },
     { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent }
+    { path: "item/:id", component: ItemDetailComponent },
+    { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
