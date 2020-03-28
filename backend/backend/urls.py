d9ea3093af731b1d9eb5f3596e6ff3851f082d88
HarsimratBhundar <harsimratsingh80@gmail.com>
2020-03-28 18:33:04 -0400

    added relative pathing for local import

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from auth.views import login
+from .auth.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
