fc640d12a7c998b112821dbe7a0109d1b9c0e9c4
HarsimratBhundar <harsimratsingh80@gmail.com>
2020-03-28 21:27:31 -0400

    created shared service folder and user service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { User } from '~/app/shared/model/user.model';
+import { alert, prompt } from "tns-core-modules/ui/dialogs";
+import { Page } from "tns-core-modules/ui/page";
+import { UserService } from '~/app/shared/service/user.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'ns-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-
+export class LoginComponent {
   isLoggingIn = true;
+  user: User;
+  @ViewChild("password") password: ElementRef;
+  @ViewChild("confirmPassword") confirmPassword: ElementRef;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private page: Page, private userService: UserService, private router: Router) {
+      this.page.actionBarHidden = true;
+      this.user = new User();
   }
 
   toggleForm() {
-    this.isLoggingIn = !this.isLoggingIn;
+      this.isLoggingIn = !this.isLoggingIn;
   }
 
   submit() {
-    if (this.isLoggingIn) {
+      if (!this.user.email || !this.user.password) {
+          this.alert("Please provide both an email address and password.");
+          return;
+      }
 
-    } else {
-      
-    }
+      if (this.isLoggingIn) {
+          this.login();
+      } else {
+          this.register();
+      }
   }
 
+  login() {
+      // this.userService.login(this.user)
+      //     .then(() => {
+      //         this.router.navigate(["/home"]);
+      //     })
+      //     .catch(() => {
+      //         this.alert("Unfortunately we could not find your account.");
+      //     });
+  }
+
+  register() {
+      // if (this.password != this.confirmPassword) {
+      //     this.alert("Your passwords do not match.");
+      //     return;
+      // }
+      // this.userService.register(this.user)
+      //     .then(() => {
+      //         this.alert("Your account was successfully created.");
+      //         this.isLoggingIn = true;
+      //     })
+      //     .catch(() => {
+      //         this.alert("Unfortunately we were unable to create your account.");
+      //     });
+  }
+
+  forgotPassword() {
+      // prompt({
+      //     title: "Forgot Password",
+      //     message: "Enter the email address you used to register for APP NAME to reset your password.",
+      //     inputType: "email",
+      //     defaultText: "",
+      //     okButtonText: "Ok",
+      //     cancelButtonText: "Cancel"
+      // }).then((data) => {
+      //     if (data.result) {
+      //         this.userService.resetPassword(data.text.trim())
+      //             .then(() => {
+      //                 this.alert("Your password was successfully reset. Please check your email for instructions on choosing a new password.");
+      //             }).catch(() => {
+      //                 this.alert("Unfortunately, an error occurred resetting your password.");
+      //             });
+      //     }
+      // });
+  }
+
+  focusPassword() {
+      this.password.nativeElement.focus();
+  }
+  focusConfirmPassword() {
+      if (!this.isLoggingIn) {
+          this.confirmPassword.nativeElement.focus();
+      }
+  }
+
+  alert(message: string) {
+      return alert({
+          title: "APP NAME",
+          okButtonText: "OK",
+          message: message
+      });
+  }
 }
---
+++ src/app/shared/service/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { User } from "../model/user.model";
+
+@Injectable()
+export class UserService {
+    register(user: User) {
+
+    }
+
+    login(user: User) {
+        
+    }
+
+    resetPassword(email) {
+    }
+
+    handleErrors(error: Error) {
+    }
+}
